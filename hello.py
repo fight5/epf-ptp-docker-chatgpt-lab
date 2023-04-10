3fb6df6974899b66eb7288ca909137006135b76f
fight5 <94685815+fight5@users.noreply.github.com>
2023-04-11 00:15:58 +0200

    update hello.py

--- hello.py
+++ hello.py
@@ -9,17 +9,30 @@ openai.api_key = os.environ.get('OPENAI_KEY')
 
 @app.route('/')
 def index():
-    return "<h1>Hello, World!</h1>"
+return "<h1>Hello, World!</h1>"
 
 @app.route('/chatgpt')
 def chatgpt():
-    args = request.args
-    message =args.get("message")
-    print(message)
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": message}]
-    )
-    return completion['choices'][0]['message']['content']
+args = request.args
+message =args.get("message")
+print(message)
+completion = openai.ChatCompletion.create(
+model="gpt-3.5-turbo",
+messages=[{"role": "user", "content": message}]
+)
+return completion['choices'][0]['message']['content']
 
 
+@app.route('/SMACEroute') 
+def SMACE():
+args = request.args
+content = request.args.get("content")
+language = request.args.get("language") 
+    
+message = content + "\nDo it in " + language 
+print(message) 
+completion = openai.ChatCompletion.create(
+model="gpt-3.5-turbo", 
+messages=[{"role": "user", "content": message}]
+)
+return completion['choices'][0]['message']['content']
